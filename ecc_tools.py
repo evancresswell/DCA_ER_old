c73d6fc170e609929c1956c21872433d40cb9c86
Cresswell-Clay <cresswellclayec@dk1202089912.nih.gov>
2020-01-17 15:32:02 -0500

    Finished comparison plots of DCA and ER across ROC, AUC and Precision-Recall. Next want to check sensitivity (in a new jupyter notebook

--- ecc_tools.py
+++ ecc_tools.py
@@ -9,6 +9,8 @@ from scipy.spatial import distance_matrix
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonWarning)
 import numpy as np
+from IPython.display import HTML
+import random
 
 def contact_map(pdb,ipdb,cols_removed):
     pdb_id = pdb[ipdb,5]
@@ -78,3 +80,36 @@ def roc_curve(ct,di,ct_thres):
     return pbin,tpbin,fpbin
 
 
+def hide_toggle(for_next=False):
+    this_cell = """$('div.cell.code_cell.rendered.selected')"""
+    next_cell = this_cell + '.next()'
+
+    toggle_text = 'Toggle show/hide'  # text shown on toggle link
+    target_cell = this_cell  # target cell to control with toggle
+    js_hide_current = ''  # bit of JS to permanently hide code in current cell (only when toggling next cell)
+
+    if for_next:
+        target_cell = next_cell
+        toggle_text += ' next cell'
+        js_hide_current = this_cell + '.find("div.input").hide();'
+
+    js_f_name = 'code_toggle_{}'.format(str(random.randint(1,2**64)))
+
+    html = """
+        <script>
+            function {f_name}() {{
+                {cell_selector}.find('div.input').toggle();
+            }}
+
+            {js_hide_current}
+        </script>
+
+        <a href="javascript:{f_name}()">{toggle_text}</a>
+    """.format(
+        f_name=js_f_name,
+        cell_selector=target_cell,
+        js_hide_current=js_hide_current, 
+        toggle_text=toggle_text
+    )
+
+    return HTML(html)
