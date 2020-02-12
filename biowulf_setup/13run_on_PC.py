dd81a939e0e438afa1a04bd7d14e7d0c49c9b671
Evan Cresswell <evancresswell@gmail.com>
2020-02-12 16:01:59 -0500

    running singularity container now works

--- biowulf_setup/13run_on_PC.py
+++ biowulf_setup/13run_on_PC.py
@@ -2,13 +2,31 @@ import os
 import timeit
 pfam_id = 'PF04542'
 pfam_id = 'PF00186'
+pfam_id = 'PF00011'
+
+start_time = timeit.default_timer()
+os.system('sudo singularity exec -B ~/DCA_ER/biowulf_setup/ ~/DCA_ER/dca_er.simg python 1main_PLM.py '+pfam_id)
+run_time = timeit.default_timer() - start_time
+print('plm run time:',run_time)
+
+"""
+start_time = timeit.default_timer()
+os.system('sudo singularity exec -B ~/DCA_ER/biowulf_setup/ ~/DCA_ER/dca_er-DCA.simg python 1main_ER.py '+pfam_id)
+run_time = timeit.default_timer() - start_time
+print('ER run time:',run_time)
+
+start_time = timeit.default_timer()
+os.system('sudo singularity exec -B ~/DCA_ER/biowulf_setup/ ~/DCA_ER/dca_er-DCA.simg python 1main_DCA.py '+pfam_id)
+run_time = timeit.default_timer() - start_time
+print('mf run time:',run_time)
+
+
 
 start_time = timeit.default_timer()
 os.system('python 1main_ER.py '+pfam_id)
 run_time = timeit.default_timer() - start_time
 print('ER run time:',run_time)
 
-"""
 start_time = timeit.default_timer()
 os.system('python 1main_PLM.py '+pfam_id)
 run_time = timeit.default_timer() - start_time
