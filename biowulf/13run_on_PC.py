49b8d2af63201c64483ec26a4417bbda08ad1db4
Evan Cresswell <evancresswell@gmail.com>
2020-02-19 16:21:59 -0500

    adding parse_pfam to generate msa and pdb ref database from Pfam

--- biowulf/13run_on_PC.py
+++ biowulf/13run_on_PC.py
@@ -5,18 +5,18 @@ pfam_id = 'PF00186'
 pfam_id = 'PF00011'
 
 start_time = timeit.default_timer()
-os.system('sudo singularity exec -B ~/DCA_ER/biowulf_setup/ ~/DCA_ER/dca_er.simg python 1main_PLM.py '+pfam_id)
+os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er.simg python 1main_PLM.py '+pfam_id)
 run_time = timeit.default_timer() - start_time
 print('plm run time:',run_time)
 
 """
 start_time = timeit.default_timer()
-os.system('sudo singularity exec -B ~/DCA_ER/biowulf_setup/ ~/DCA_ER/dca_er-DCA.simg python 1main_ER.py '+pfam_id)
+os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_ER.py '+pfam_id)
 run_time = timeit.default_timer() - start_time
 print('ER run time:',run_time)
 
 start_time = timeit.default_timer()
-os.system('sudo singularity exec -B ~/DCA_ER/biowulf_setup/ ~/DCA_ER/dca_er-DCA.simg python 1main_DCA.py '+pfam_id)
+os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_DCA.py '+pfam_id)
 run_time = timeit.default_timer() - start_time
 print('mf run time:',run_time)
 
