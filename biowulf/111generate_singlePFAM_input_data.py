835f8451c8a0d4600914242c4b3dd742d724376b
Evan Chesapeake Cresswell-Clay <cresswellclayec@biowulf.nih.gov>
2020-03-02 11:48:55 -0500

    compiling list of empty pdb references

--- biowulf/111generate_singlePFAM_input_data.py
+++ biowulf/111generate_singlePFAM_input_data.py
@@ -15,6 +15,11 @@ os.system('mkdir DI/PLM')
 
 print("Generating Data for Protein:\n",pfam_id)
 
+# Create list file for missing pdb structures
+if os.path.exists('missing_PDB.txt'):
+	file_missing_pdb = open("missing_PDB.txt",'w')
+	file_missing_pdb.close()
+
 def generate_pfam_data(pfam_id):
     	data_path = '../../hoangd2_data/Pfam-A.full'
 	pdb = np.load('%s/%s/pdb_refs.npy'%(data_path,pfam_id))
@@ -23,27 +28,32 @@ def generate_pfam_data(pfam_id):
 	# delete 'b' in front of letters (python 2 --> python 3)
 	print(pdb.shape)	
 	print(pdb)	
-	pdb = np.array([pdb[t,i].decode('UTF-8') for t in range(pdb.shape[0]) \
-			 for i in range(pdb.shape[1])]).reshape(pdb.shape[0],pdb.shape[1])
-	
+	if len(pdb) == 0:
+		print("Missing PDB structure")
+		file_missing_pdb = open("missing_PDB.txt",'a')
+		file_missing_pdb.write("%s\n"% pfam_id)
+		file_missing_pdb.close()
+	else:	
+		pdb = np.array([pdb[t,i].decode('UTF-8') for t in range(pdb.shape[0]) \
+				 for i in range(pdb.shape[1])]).reshape(pdb.shape[0],pdb.shape[1])
 
-	# Create pandas dataframe for protein structure
-	df = pd.DataFrame(pdb,columns = ['PF','seq','id','uniprot_start','uniprot_start',\
-									 'pdb_id','chain','pdb_start','pdb_end'])
-	print(df.head())
+		# Create pandas dataframe for protein structure
+		df = pd.DataFrame(pdb,columns = ['PF','seq','id','uniprot_start','uniprot_start',\
+										 'pdb_id','chain','pdb_start','pdb_end'])
+		print(df.head())
 
-	ipdb = 0
-	print('seq:',int(pdb[ipdb,1]))
+		ipdb = 0
+		print('seq:',int(pdb[ipdb,1]))
 
-	# data processing
-	s0,cols_removed,s_index,s_ipdb = dp.data_processing(data_path,pfam_id,ipdb,\
-					gap_seqs=0.2,gap_cols=0.2,prob_low=0.004,conserved_cols=0.9)
-	# Save processed data
-	msa_outfile, ref_outfile = dp.write_FASTA(s0,pfam_id,s_ipdb,path='pfam_ecc/')	
+		# data processing
+		s0,cols_removed,s_index,s_ipdb = dp.data_processing(data_path,pfam_id,ipdb,\
+						gap_seqs=0.2,gap_cols=0.2,prob_low=0.004,conserved_cols=0.9)
+		# Save processed data
+		msa_outfile, ref_outfile = dp.write_FASTA(s0,pfam_id,s_ipdb,path='pfam_ecc/')	
 
-	np.savetxt('pfam_ecc/%s_s0.txt'%(pfam_id),s0)	
-	np.savetxt('pfam_ecc/%s_s_index.txt'%(pfam_id),s_index)	
-    	return
+		np.savetxt('pfam_ecc/%s_s0.txt'%(pfam_id),s0)	
+		np.savetxt('pfam_ecc/%s_s_index.txt'%(pfam_id),s_index)	
+		return
 
 #-------------------------------
 
