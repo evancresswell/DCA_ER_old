e2b0c2d2c66382e470322633099ff6de35127414
Evan Cresswell-Clay <evancresswell@gmail.com>
2020-03-25 16:22:51 -0400

    Dockerfile works on biowulf

--- biowulf/13run_on_PC.py
+++ biowulf/13run_on_PC.py
@@ -9,19 +9,23 @@ pfam_id = 'PF00186'
 on_pc = True
 if on_pc:
 	start_time = timeit.default_timer()
-	os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_PLM.py '+pfam_id)
+	#os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_PLM.py '+pfam_id)
 	run_time = timeit.default_timer() - start_time
 	print('plm run time:',run_time)
 
 	start_time = timeit.default_timer()
-	os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_ER.py '+pfam_id)
+	#os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_ER.py '+pfam_id)
 	run_time = timeit.default_timer() - start_time
 	print('ER run time:',run_time)
 
 	start_time = timeit.default_timer()
-	os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_DCA.py '+pfam_id)
+	#os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_DCA.py '+pfam_id)
 	run_time = timeit.default_timer() - start_time
 	print('mf run time:',run_time)
+
+	print('Plotting....')	
+	os.system('sudo singularity exec -B ~/DCA_ER/biowulf/ ~/DCA_ER/dca_er-DCA.simg python 1main_DCA.py '+pfam_id)
+	print('...done')	
 else:
 	os.system('module load singularity')
 	start_time = timeit.default_timer()
